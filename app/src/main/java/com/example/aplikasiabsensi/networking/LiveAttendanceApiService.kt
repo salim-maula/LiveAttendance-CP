262290fe6832808098f990bfc8ec8a4932532a1d
salim-maula <salim-maula>
2022-03-23 16:20:10 +0700

    feat: Add forgot passwod

--- app/src/main/java/com/example/aplikasiabsensi/networking/LiveAttendanceApiService.kt
+++ app/src/main/java/com/example/aplikasiabsensi/networking/LiveAttendanceApiService.kt
@@ -1,6 +1,8 @@
 package com.example.aplikasiabsensi.networking
 
+import com.example.aplikasiabsensi.model.ForgotPasswordResponse
 import com.example.aplikasiabsensi.model.LoginResponse
+import com.example.aplikasiabsensi.views.forgotpass.ForgotPasswordRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
@@ -10,4 +12,8 @@ interface LiveAttendanceApiService {
     @Headers("Accept: application/json", "Content-Type: application/json")
     @POST("auth/login")
     fun loginRequest(@Body body: String): Call<LoginResponse>
+
+    @Headers("Accept: application/json", "Content-Type: application/json")
+    @POST("auth/password/forgot")
+    fun forgotPasswordRequest(@Body body: String): Call<ForgotPasswordResponse>
 }
--- app/src/main/java/com/example/aplikasiabsensi/views/forgotpass/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/aplikasiabsensi/views/forgotpass/ForgotPasswordActivity.kt
@@ -2,7 +2,25 @@ package com.example.aplikasiabsensi.views.forgotpass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.util.Patterns
+import com.example.aplikasiabsensi.R
 import com.example.aplikasiabsensi.databinding.ActivityForgotPasswordBinding
+import com.example.aplikasiabsensi.dialog.MyDialog
+import com.example.aplikasiabsensi.model.ForgotPasswordResponse
+import com.example.aplikasiabsensi.model.LoginResponse
+import com.example.aplikasiabsensi.networking.ApiServices
+import com.example.aplikasiabsensi.networking.RetrofitClient
+import com.example.aplikasiabsensi.views.login.LoginActivity
+import com.google.gson.Gson
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Converter
+import retrofit2.Response
+import java.io.IOException
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
@@ -21,10 +39,93 @@ class ForgotPasswordActivity : AppCompatActivity() {
         binding.tbForgotPassword.setNavigationOnClickListener {
             finish()
         }
+
+        binding.btnForgotPassword.setOnClickListener {
+            val email = binding.etEmailForgotPassword.text.toString()
+            if (isFormValid(email)) {
+                forgotPassToServer(email)
+            }
+        }
     }
 
-    private fun init(){
+    private fun isFormValid(email: String): Boolean {
+        if (email.isEmpty()) {
+            binding.etEmailForgotPassword.error = getString(R.string.please_field_your_email)
+            binding.etEmailForgotPassword.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.etEmailForgotPassword.error = getString(R.string.please_use_valid_email)
+            binding.etEmailForgotPassword.requestFocus()
+        } else {
+            binding.etEmailForgotPassword.error = null
+            return true
+        }
+        return false
+    }
+
+    private fun forgotPassToServer(email: String) {
+        val forgotPasswordRequest = ForgotPasswordRequest(email = email)
+        val forgotPasswordRequestString = Gson().toJson(forgotPasswordRequest)
+
+        MyDialog.showProgressDialog(this)
+
+        ApiServices.getLiveAttendance()
+            .forgotPasswordRequest(forgotPasswordRequestString)
+            .enqueue(object : Callback<ForgotPasswordResponse> {
+                override fun onResponse(
+                    call: Call<ForgotPasswordResponse>,
+                    response: Response<ForgotPasswordResponse>
+                ) {
+                    MyDialog.hideDialog()
+                    if (response.isSuccessful) {
+                        val message = response.body()?.message
+                        MyDialog.dynamicDialog(
+                            this@ForgotPasswordActivity,
+                            getString(R.string.success),
+                            message.toString()
+                        )
+                        Handler(Looper.getMainLooper()).postDelayed({
+                         MyDialog.hideDialog()
+                            finish()
+                        },2000)
+                    }else{
+                        val errorConverter: Converter<ResponseBody, ForgotPasswordResponse> =
+                            RetrofitClient
+                                .getClient()
+                                .responseBodyConverter(
+                                    ForgotPasswordResponse::class.java,
+                                    arrayOfNulls<Annotation>(0)
+                                )
+                        var errorResponse: ForgotPasswordResponse?
+                        try {
+                            response.errorBody()?.let {
+                                errorResponse = errorConverter.convert(it)
+                                MyDialog.dynamicDialog(this@ForgotPasswordActivity,
+                                    getString(R.string.failed),
+                                    errorResponse?.message.toString()
+                                )
+                            }
+                        }catch (e: IOException){
+                            e.printStackTrace()
+                            Log.e(TAG, "Error: ${e.message}")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<ForgotPasswordResponse>, t: Throwable) {
+                    MyDialog.hideDialog()
+                    Log.e(TAG, "Error: ${t.message}")
+                }
+
+            })
+
+    }
+
+    private fun init() {
         setSupportActionBar(binding.tbForgotPassword)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    companion object {
+        private val TAG = ForgotPasswordActivity::class.java.simpleName
     }
 }
